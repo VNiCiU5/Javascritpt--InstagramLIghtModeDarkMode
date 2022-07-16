7144649f8b5d0acadd9bfa89414ffe8d28562bb8
Vinicius <viniciuspontesdoliveira@gmail.com>
2022-07-16 16:07:28 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,65 @@
+function changeModeDark() {
+	changeClassesDark();
+	changeText();
+}
+
+function changeModeLight() {
+	changeClassesLight();
+	changeText();
+}
+
+
+
+function changeClassesDark() {
+	button.classList.toggle(darkModeClass);
+	h1.classList.toggle(darkModeClass);
+	body.classList.toggle(darkModeClass);
+	footer.classList.toggle(darkModeClass);
+	changeModeLight();
+}
+
+function changeClassesLight() {
+	button.classList.toggle(LightModeClass);
+	h1.classList.toggle(LightModeClass);
+	body.classList.toggle(LightModeClass);
+	footer.classList.toggle(LightModeClass);
+	changeModeDark();
+	
+}
+
+
+function changeClasses() {
+	button.classList.toggle(darkModeClass);
+	h1.classList.toggle(darkModeClass);
+	body.classList.toggle(darkModeClass);
+	footer.classList.toggle(darkModeClass);
+	
+}
+
+function changeText() {
+	const lightMode = 'Light Mode';
+	const darkMode = 'Dark Mode';
+
+	if (body.classList.contains(darkModeClass)) {
+		button.innerHTML = lightMode;
+		h1.innerHTML = darkMode + ' ON';
+		return;
+	}
+
+	button.innerHTML = darkMode;
+	h1.innerHTML = lightMode + ' ON';
+}
+
+const LightModeClass = 'light-mode';
+const darkModeClass = 'dark-mode';
+const button = document.getElementById('mode-selector');
+const h1 = document.getElementById('page-title');
+const body = document.getElementsByTagName('body')[0];
+const footer = document.getElementsByTagName('footer')[0];
+
+
+
+
+
+
+button.addEventListener('click', changeModeDark);
